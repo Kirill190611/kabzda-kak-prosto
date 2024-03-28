5616c0961b69d0689df8df873e82dd277563b74a
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-03-28 21:46:32 +0300

    First practice with Storybook

---
+++ src/components/accordion/Accordion.stories.tsx
@@ -0,0 +1,47 @@
+import {action} from '@storybook/addon-actions';
+
+import {Accordion} from './Accordion';
+import {useState} from "react";
+
+export default  {
+    component: Accordion,
+};
+
+/*type Story = StoryObj<typeof Accordion>
+
+export const FirstStory: Story = {
+    args: {
+        accordionTitle: "hello",
+        collapsed: true,
+    },
+}*/
+
+const onChangeHandler = action('onChange')
+
+export const CollapsedAccordion = () => {
+    return (
+        <Accordion accordionTitle="Collapsed Accordion"
+                   collapsed={true}
+                   onClick={onChangeHandler}/>
+    )
+}
+
+export const OpenedAccordion = () => {
+    return (
+        <Accordion accordionTitle="Opened Accordion"
+                   collapsed={false}
+                   onClick={() => {}}/>
+    )
+}
+
+export const AccordionDemo = () => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    return (
+        <Accordion accordionTitle="Accordion Demo"
+                   collapsed={collapsed}
+                   onClick={() => {
+                       setCollapsed(!collapsed)
+                   }}/>
+    )
+}
