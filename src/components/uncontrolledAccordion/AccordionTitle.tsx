fc4d1ea632f1158a90b1e99bbf7386ca54aed6de
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-04-14 22:06:33 +0300

    Practice with custom select component

--- src/components/uncontrolledAccordion/AccordionTitle.tsx
+++ src/components/uncontrolledAccordion/AccordionTitle.tsx
@@ -3,20 +3,22 @@ import React from "react";
 type AccordionTitlePropsType = {
     title: string
     collapsed: boolean
-    setCollapsed: (collapsed: boolean) => void
+    setCollapsed?: (collapsed: boolean) => void
+    onClick: () => void
 }
 export const AccordionTitle = ({
                                    title,
                                    setCollapsed,
-                                   collapsed
+                                   collapsed,
+                                   onClick
                                }: AccordionTitlePropsType) => {
     console.log("AccordionTitle rendering")
 
-    const onCollapsed = () => {
+    /*const onCollapsed = () => {
         setCollapsed(!collapsed)
-    }
+    }*/
 
     return (
-        <h3 onClick={onCollapsed}>{title}</h3>
+        <h3 onClick={onClick}>{title}</h3>
     );
 }
--- src/components/uncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/uncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,22 +1,49 @@
-import React, {useState} from "react";
+import React, {useReducer, useState} from "react";
 import {AccordionTitle} from "./AccordionTitle";
 import {UncontrolledAccordionBody} from "./UncontrolledAccordionBody";
 
 type AccordionPropsType = {
     accordionTitle: string
 }
+
+type ActionType = {
+    type: string
+}
+
+const TOGGLE_CONSTANT = "TOGGLE-COLLAPSED"
+
+const reducer = (state: boolean, action: ActionType) => {
+    switch (action.type) {
+        case TOGGLE_CONSTANT:
+            return !state;
+        default:
+            throw new Error("Incorrect action type")
+    }
+    /*if (action.type === TOGGLE_CONSTANT) {
+        return !state
+    }
+    return state;*/
+}
 export const UncontrolledAccordion = ({accordionTitle}: AccordionPropsType) => {
     console.log("UncontrolledAccordion rendered")
 
-    const [collapsed, setCollapsed] = useState(false);
+    // const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, dispatch] = useReducer(reducer, false);
 
     return (
         <div>
-            <AccordionTitle title={accordionTitle}
+            {/*<AccordionTitle title={accordionTitle}
                             setCollapsed={setCollapsed}
+                            collapsed={collapsed}/>*/}
+            <AccordionTitle title={accordionTitle}
+                            onClick={() => {
+                                dispatch({type: TOGGLE_CONSTANT})
+                            }}
                             collapsed={collapsed}/>
             {!collapsed && <UncontrolledAccordionBody/>}
         </div>
     );
 }
+
+//todo: start 16v from 28:10
 
